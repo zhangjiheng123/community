c682af49b677e689c55ca446a8eec4e65ebb1423
张继衡 <you@example.com>
2020-03-02 11:07:56 +0800

    int repo

---
+++ src/test/java/com/example/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.community;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
